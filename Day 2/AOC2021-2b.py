dd10c6b013365da6f50e42958a5b5409749ab9dd
Brian Luc <brianluc@@transport.nsw.gov.au>
2021-12-02 23:16:32 +1100

    Advent of Code 2021 Day 2 Part b

---
+++ Day 2/AOC2021-2b.py
@@ -0,0 +1,25 @@
+# Advent of Code Day 2 Part 2
+def replaceAndConvert(cmd):
+    cmd = cmd.replace('\n', '')
+    return cmd
+
+f = open('input.txt', 'r')
+commands = f.readlines()
+commands = map(replaceAndConvert, commands)
+depth = 0
+horizontal = 0
+aim = 0
+
+for command in commands:
+    command = command.split(' ')
+    direction = command[0]
+    distance = int(command[1])
+    if direction == 'up':
+        aim -= distance
+    elif direction == 'down':
+        aim += distance
+    elif direction == 'forward':
+        horizontal += distance
+        depth += (aim * distance)
+
+print(horizontal * depth)
